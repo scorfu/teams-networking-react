d91a8dcd10e1c723b03fa1cc59e9dd7a40751f65
scorfu <sergiucorfu@yahoo.com>
2021-03-22 20:04:06 +0200

    add component as class + internal state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
+import { Component } from 'react';
 import './App.css';
 import { TeamsTable } from "./TeamsTable";
 
-let teams = [
-  {
-    "name": "test",
-    "members": "test test",
-    "url": "https://github.com/scorfu/scorfu.github.io"
-  },
-  {
-    "name": "teams-networking",
-    "members": "test1 test1",
-    "url": "https://github.com/scorfu"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teams: []
+    }
   }
-];
+  
+componentDidMount() {
+  console.warn('mount');
+  setTimeout(() => {
+    this.setState({
+      teams: [
+        {
+          "name": "test",
+          "members": "test test",
+          "url": "https://github.com/scorfu/scorfu.github.io"
+        },
+        {
+          "name": "teams-networking",
+          "members": "test1 test1",
+          "url": "https://github.com/scorfu"
+        }
+      ]
+    })
+    console.warn('loaded')
+  }, 2000);
+}
 
-function App() {
-  return (
-    <div>
-      <h1>Teams Networking</h1>
-      <div>Search</div>
-      <TeamsTable teams={teams} border={1} />
-    </div>
-  );
+  render() {
+    console.debug(this.state.teams)
+    return (
+      <div>
+        <h1>Teams Networking</h1>
+        <div>Search</div>
+        <TeamsTable teams={this.state.teams} border={1} />
+      </div>
+    );
+  }
 }
 
 export default App;
