b6b45f05c4fe4354124e8df756cb921bd9d59ed2
scorfu <sergiucorfu@yahoo.com>
2021-03-22 19:32:07 +0200

    made it work & add border prop to table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import { TeamsTable } from "./TeamsTable";
+
+let teams = [
+  {
+    "name": "test",
+    "members": "test test",
+    "url": "https://github.com/scorfu/scorfu.github.io"
+  },
+  {
+    "name": "teams-networking",
+    "members": "test1 test1",
+    "url": "https://github.com/scorfu"
+  }
+];
 
 function App() {
   return (
     <div>
       <h1>Teams Networking</h1>
       <div>Search</div>
-      <p>Table</p>
+      <TeamsTable teams={teams} border={1} />
     </div>
   );
 }
